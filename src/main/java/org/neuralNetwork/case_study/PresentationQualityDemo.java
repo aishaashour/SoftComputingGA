86b16fb4c693ac133b12c807451f06ed4dba0665
shaza <shazawagdy@gmail.com>
2025-12-19 21:45:34 +0200

    minor modifications

--- src/main/java/org/neuralNetwork/case_study/PresentationQualityDemo.java
+++ src/main/java/org/neuralNetwork/case_study/PresentationQualityDemo.java
@@ -17,26 +17,26 @@ public class PresentationQualityDemo {
         double[][] yTrain = data.y;
 
         //build Neural Network (intialization)
-        NeuralNetwork nn = new NeuralNetwork();
+        NeuralNetwork ANN = new NeuralNetwork();
         UniformXavierInitializer initializer = new UniformXavierInitializer();
 
         double[][] w1 = initializer.initialize(4, 8);
         double[] b1 = new double[8];
-        nn.addLayer(new DenseLayer(4, 8, new RectifiedLinearUnitFunction(), w1, b1));
+        ANN.addLayer(new DenseLayer(4, 8, new RectifiedLinearUnitFunction(), w1, b1));
         double[][] w2 = initializer.initialize(8, 1);
         double[] b2 = new double[1];
-        nn.addLayer(new DenseLayer(8, 1, new SigmoidFunction(), w2, b2));
-        nn.setLossFunction(new BinaryCross_EntropyLossFunction());
+        ANN.addLayer(new DenseLayer(8, 1, new SigmoidFunction(), w2, b2));
+        ANN.setLossFunction(new BinaryCross_EntropyLossFunction());
 
         // Train
-        nn.train(xTrain, yTrain, 1000, 2, 0.05);
+        ANN.train(xTrain, yTrain, 1000, 2, 0.05);
 
         // testing
         //bad presentation {0.30, 0.75, 0.20, 0.25}
         //average presentation  {0.60, 0.40, 0.55, 0.50}
         //good representation {0.85, 0.15, 0.9, 0.88}
         double[][] testSample = {{0.85, 0.15, 0.9, 0.88}};
-        double[][] prediction = nn.predict(testSample);
+        double[][] prediction = ANN.predict(testSample);
         System.out.println("Probability of GOOD presentation = " + prediction[0][0]);
     }
 }
